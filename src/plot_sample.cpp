cc0e3637b12e5c8fc16b963cf0aae2d061dbf65c
Dude19735 <dumeni.manatschal@gmail.com>
2023-10-20 11:51:09 +0200

    change CMakeLists.txt and add a sample for matplotplusplus

---
+++ src/plot_sample.cpp
@@ -0,0 +1,20 @@
+// include matplotplusplus
+// NOTE: use <..> and not ".."!!
+#include <matplot/matplot.h>
+
+// add a main (one is required for every build target)
+int main(int argc, char** argv){
+
+    std::vector<double> x = matplot::linspace(0, 2 * matplot::pi);
+    std::vector<double> y = matplot::transform(x, [](auto x) { return sin(x); });
+
+    matplot::plot(x, y, "-*");
+    matplot::hold(matplot::on);
+    matplot::plot(x, matplot::transform(y, [](auto y) { return -y; }), "--xr");
+    matplot::plot(x, matplot::transform(x, [](auto x) { return x / matplot::pi - 1.; }), "-:gs");
+    matplot::plot({1.0, 0.7, 0.4, 0.0, -0.4, -0.7, -1}, "k");
+
+    matplot::show();
+    
+    return 0;
+}
