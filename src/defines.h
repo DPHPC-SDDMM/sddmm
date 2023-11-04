2db903eece6fc22684bea49ccd6c2cf83e99a3ef
Dumeni Manatschal <dumeni.manatschal@gmail.com>
2023-11-04 02:19:23 +0100

    add distinction for operating systems to set op dependent constants like path separator

--- src/defines.h
+++ src/defines.h
@@ -30,6 +30,8 @@ namespace SDDMM {
     */
     class Defines {
     public:
+        static constexpr SDDMM::Types::expmt_t epsilon = 1e-6;
+
         struct InitParams {
             int sampleParam;
         };
@@ -59,5 +61,46 @@ namespace SDDMM {
             std::string lines = " =============================== ";
             return lines + name + lines;
         }
+
+        // some generic way to distinguish operating systems
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    static const char path_separator = '\\';
+   //define something for Windows (32-bit and 64-bit, this part is common)
+   #ifdef _WIN64
+      //define something for Windows (64-bit only)
+	  #define PLATFORM_WINDOWS_x64
+   #else
+      //define something for Windows (32-bit only)
+	  #define PLATFORM_WINDOWS_x32
+   #endif
+#elif __APPLE__
+    static const char path_separator = '/';
+    #include <TargetConditionals.h>
+    #if TARGET_IPHONE_SIMULATOR
+         // iOS, tvOS, or watchOS Simulator
+    #elif TARGET_OS_MACCATALYST
+         // Mac's Catalyst (ports iOS API into Mac, like UIKit).
+    #elif TARGET_OS_IPHONE
+        // iOS, tvOS, or watchOS device
+    #elif TARGET_OS_MAC
+        // Other kinds of Apple platforms
+		#define PLATFORM_MAC
+    #else
+    #   error "Unknown Apple platform"
+    #endif
+#elif __ANDROID__
+    // Below __linux__ check should be enough to handle Android,
+    // but something may be unique to Android.
+#elif __linux__
+    // linux
+	#define PLATFORM_LINUX
+    static const char path_separator = '/';
+#elif __unix__ // all unices not caught above
+    // Unix
+#elif defined(_POSIX_VERSION)
+    // POSIX
+#else
+#   error "Unknown compiler"
+#endif
     };
 }
